563a5c8024863934b17284173e30c9a4ca7ecdf6
Ludwig <liudvikas.nemiro.2018@uni.strath.ac.uk>
2020-11-11 20:14:05 +0000

    Added functionality to print threads as well as threadGroups

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         ThreadInfoPrinter threadInfoPrinter = new ThreadInfoPrinter();
-        threadInfoPrinter.printInfo();
+        System.out.println("Hierarchy Format:\n");
+        threadInfoPrinter.printInfo(0);
+        System.out.println();
+        System.out.println("Listed Format:\n");
+        threadInfoPrinter.printInfo(1);
     }
 }
--- src/ThreadInfoPrinter.java
+++ src/ThreadInfoPrinter.java
@@ -1,11 +1,50 @@
 public class ThreadInfoPrinter extends Thread{
 
     //method that prints info about a ThreadGroup and its threads
-    public void printInfo() {
+    public void printInfo(int format) {
         ThreadGroup[] threadGroups = getAllThreadGroups();
+        Thread[] threads = getAllThreads(getRootThreadGroup());
+        switch(format){
+            case 0:
+                // Print in hierarchy format, i.e. print thread group with all child threads/thread groups beneath it
+                printHierarchy(threadGroups[0],"",threadGroups,threads);
+                break;
+            case 1:
+                // Print in Listed format, i.e. print all thread groups, then all threads
+                for(ThreadGroup a: threadGroups){
+                    System.out.println(a.toString());
+                }
+                for(Thread t: threads){
+                    System.out.println(t.toString());
+                }
+                break;
+            default:
+                System.err.println("Warning: Invalid format value!");
 
-        for(ThreadGroup a: threadGroups){
-            System.out.println(a.toString());
+
+        }
+
+    }
+
+    private void printHierarchy(ThreadGroup currentGroup, String indent, ThreadGroup[] threadGroups, Thread[] threads){
+        //Display current ThreadGroup
+        System.out.println(indent + currentGroup.toString());
+        indent += "\t";
+
+        //Display current ThreadGroups threads
+        for(Thread t: threads){
+            if(t.getThreadGroup().equals(currentGroup)){
+                System.out.println(indent + t.toString());
+            }
+        }
+
+        //Display current ThreadGroups child groups and their associated threads
+        for(ThreadGroup g: threadGroups){
+            if(!g.equals(getRootThreadGroup())){
+                if(g.getParent().equals(currentGroup)){
+                    printHierarchy(g,indent,threadGroups,threads);
+                }
+            }
 
         }
     }
@@ -32,7 +71,7 @@ public class ThreadInfoPrinter extends Thread{
 
     //create new ThreadGroups list that includes root
     public ThreadGroup[] getAllThreadGroups(){
-        ThreadGroup[]threadGroupsAfterRoot = getThreadGroupsAfterRoot();
+        ThreadGroup[] threadGroupsAfterRoot = getThreadGroupsAfterRoot();
         ThreadGroup[] allThreadGroups = new ThreadGroup[threadGroupsAfterRoot.length + 1];
 
         allThreadGroups[0] = getRootThreadGroup();
@@ -44,4 +83,9 @@ public class ThreadInfoPrinter extends Thread{
         return allThreadGroups;
     }
 
+    public Thread[] getAllThreads(ThreadGroup rootThreadGroup){
+        Thread[] threads = new Thread[rootThreadGroup.activeCount()];
+        rootThreadGroup.enumerate(threads);
+        return threads;
+    }
 }
